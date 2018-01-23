91d1d63705688af0c0727f084817e4fc8924edae
Roland Milano <34466293+happylanz@users.noreply.github.com>
2018-01-23 14:23:15 +0800

    Create Rotate3.cs

---
+++ Rotate3.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, Time.deltaTime * 150f, Time.deltaTime * 150f));
+    }
+}
